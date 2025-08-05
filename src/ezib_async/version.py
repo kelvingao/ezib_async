f6de41145a0a962ce77051ac4d244367a48209ee
Kelvin Gao <89156201@qq.com>
2025-08-05 14:55:48 +0800

    bump: version 0.2.0-dev → 0.3.0-dev

--- src/ezib_async/version.py
+++ src/ezib_async/version.py
@@ -2,4 +2,4 @@
 Version information for the ezib_async package.
 """
 
-__version__ = "0.2.0-dev"  # Development version following 0.1.0 release
+__version__ = "0.3.0-dev"  # Development version following 0.1.0 release
